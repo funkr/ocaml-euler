839eed71a9a57bd8b70fc3cadfb074c1fbe92e14
fro <roman.funk@ti8m.ch>
2024-06-21 16:57:14 +0200

    feat: Euler60 started

--- eulerp/eulerp.ml
+++ eulerp/eulerp.ml
@@ -1,3 +1,5 @@
 module Euler57 = Euler57
-
+module Euler60 = Euler60
+  
 let euler57 = Euler57.euler57
+let euler60 = Euler60.euler60
--- eulerp/eulerp.mli
+++ eulerp/eulerp.mli
@@ -1,3 +1,4 @@
 open Base
 
 val euler57 : unit -> string
+val euler60 : unit -> string
